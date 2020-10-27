cb6933bf961540192a93a7d3d4f97c638a6d7297
Equipo Sin Oficina <videos.notion@gmail.com>
2020-10-27 07:44:37 +0100

    deleted config file

--- index.js
+++ index.js
@@ -128,30 +128,6 @@ function parseBody (element) {
       history.replaceState(history.state, '', '/' + slug);
     }
   }
-  function onDark() {
-    el.innerHTML = '<div style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgb(46, 170, 220); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(12px) translateY(0px);"></div></div></div></div>';
-    document.body.classList.add('dark');
-    __console.environment.ThemeStore.setState({ mode: 'dark' });
-  };
-  function onLight() {
-    el.innerHTML = '<div style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgba(135, 131, 120, 0.3); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(0px) translateY(0px);"></div></div></div></div>';
-    document.body.classList.remove('dark');
-    __console.environment.ThemeStore.setState({ mode: 'light' });
-  }
-  function toggle() {
-    if (document.body.classList.contains('dark')) {
-      onLight();
-    } else {
-      onDark();
-    }
-  }
-  function addDarkModeButton(device) {
-    const nav = device === 'web' ? document.querySelector('.notion-topbar').firstChild : document.querySelector('.notion-topbar-mobile');
-    el.className = 'toggle-mode';
-    el.addEventListener('click', toggle);
-    nav.appendChild(el);
-    onLight();
-  }
   const observer = new MutationObserver(function() {
     if (redirected) return;
     const nav = document.querySelector('.notion-topbar');
@@ -160,7 +136,6 @@ function parseBody (element) {
       || mobileNav && mobileNav.firstChild) {
       redirected = true;
       updateSlug();
-      addDarkModeButton(nav ? 'web' : 'mobile');
       const onpopstate = window.onpopstate;
       window.onpopstate = function() {
         if (slugs.includes(getSlug())) {
